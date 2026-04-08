53a760f09385e9c75a5b67bde7bedf83dbcbb428
Nicholas.A.Crabtree <Nicholas.a.crabtree@gmail.com>
2026-04-08 17:03:30 -0400

    Planned addition to schema.sql documented in file

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,3 +1,5 @@
+--Planned additions include colums for XP and leveling for the characters
+
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
@@ -16,4 +18,10 @@ CREATE TABLE characters (
   dexterity INTEGER NOT NULL,
   intelligence INTEGER NOT NULL,
   created_at TIMESTAMP DEFAULT NOW()
+  
 )
+
+-- planned columns for XP + leveling (run ALTER TABLE when ready to add)
+-- ALTER TABLE characters ADD COLUMN levelINTEGER NOT NULL DEFAULT 1;
+-- ALTER TABLE characters ADD COLUMN xp INTEGER NOT NULL DEFAULT 0;
+-- ALTER TABLE characters ADD COLUMN ability_points_available INTEGER NOT NULL DEFAULT 0;
